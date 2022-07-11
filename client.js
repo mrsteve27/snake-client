e287df85742e7bfebf8c1fa16b698183a9f6d595
mrsteve27 <stevecontreras.sc27@gmail.com>
2022-07-11 09:08:07 -0400

    ...

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+const { IP, PORT } = require('./constants');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT
+  });
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write('Name: SC');
+  });
+
+  conn.on("data", (data) => {
+    console.log("received", data);
+  });
+  
+  conn.setEncoding('utf8');
+
+  return conn;
+};
+module.exports = connect;
---
+++ input.js
@@ -0,0 +1,34 @@
+
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === 'p') {
+    connection.write('Say: poop!');
+  }
+};
+module.exports = setupInput;
